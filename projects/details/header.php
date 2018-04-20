e3c90f69fd5b2c3f0b5bdf127151de84c356cffd
Ketan Desai <ketan.desai@qleverlabs.com>
2018-04-20 16:06:47 +0530

    project nav menu changes

--- projects/details/header.php
+++ projects/details/header.php
@@ -6,19 +6,19 @@
             </form><a class="navigation--close">×</a>
             <div class="navigation--row">
                 <ul class="navigation--main page-container">
-                    <li><a href="/planquadrant-edit/en/index.php" class="navigation--main-link ">Start</a></li>
-                    <li><a href="/planquadrant-edit/en/profile/index.php" class="navigation--main-link">Profile</a></li>
-                    <li><a href="/planquadrant-edit/en/projects/index.php" class="navigation--main-link active">Projects</a></li>
-                    <li><a href="/planquadrant-edit/en/people/index.php" class="navigation--main-link">People</a></li>
-                    <li><a href="/planquadrant-edit/en/pin-board/news/index.php" class="navigation--main-link">Pin board</a>
-                    <li><a href="/planquadrant-edit/en/contact-form/contact.php" class="navigation--main-link">Contact Us</a>
+                    <li><a href="/planquadrant/index.php" class="navigation--main-link ">Start</a></li>
+                    <li><a href="/planquadrant/profile/index.php" class="navigation--main-link">Profile</a></li>
+                    <li><a href="/planquadrant/projects/index.php" class="navigation--main-link active">Projects</a></li>
+                    <li><a href="/planquadrant/people/index.php" class="navigation--main-link">People</a></li>
+                    <li><a href="/planquadrant/pin-board/news/index.php" class="navigation--main-link">Pin board</a>
+                    <li><a href="/planquadrant/contact-form/contact.php" class="navigation--main-link">Contact Us</a>
                 </ul>
             </div>
         </nav>
     </div>
     <header class="main-header">
         <div class="page-container logo-container">
-            <a href="index.php#showIntro" class="logo"><img src="/planquadrant-edit/en/img/logo.png" alt="Planquadrat"></a>
+            <a href="index.php#showIntro" class="logo"><img src="/planquadrant/img/logo.png" alt="Planquadrat"></a>
         </div><a class="navigation--toggle">Menu</a>
         <nav class="navigation">
             <form action="http://www.planquadrat.com/en/suche/" class="navigation--search-form">
@@ -27,12 +27,12 @@
             </form><a href="#" class="navigation--close">×</a>
             <div class="navigation--row">
                 <ul class="navigation--main page-container">
-                    <li><a href="/planquadrant-edit/en/index.php" class="navigation--main-link ">Start</a></li>
-                    <li><a href="/planquadrant-edit/en/profile/index.php" class="navigation--main-link">Profile</a></li>
-                    <li><a href="/planquadrant-edit/en/projects/index.php" class="navigation--main-link active">Projects</a></li>
-                    <li><a href="/planquadrant-edit/en/people/index.php" class="navigation--main-link">People</a></li>
-                    <li><a href="/planquadrant-edit/en/pin-board/news/index.php" class="navigation--main-link ">Pin board</a>
-                        <li><a href="/planquadrant-edit/en/contact-form/contact.php" class="navigation--main-link">Contact Us</a>
+                    <li><a href="/planquadrant/index.php" class="navigation--main-link ">Start</a></li>
+                    <li><a href="/planquadrant/profile/index.php" class="navigation--main-link">Profile</a></li>
+                    <li><a href="/planquadrant/projects/index.php" class="navigation--main-link active">Projects</a></li>
+                    <li><a href="/planquadrant/people/index.php" class="navigation--main-link">People</a></li>
+                    <li><a href="/planquadrant/pin-board/news/index.php" class="navigation--main-link ">Pin board</a>
+                        <li><a href="/planquadrant/contact-form/contact.php" class="navigation--main-link">Contact Us</a>
                 </ul>
             </div>
         </nav>
